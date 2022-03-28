bd1c1ddcfc9e521da3f3e295d65621499444ac98
Bob Kearns <kearnsr135@gmail.com>
2022-03-28 18:10:46 +0000

    add translator logic and begin test cases

---
+++ final_project/machinetranslation/tests.py
@@ -0,0 +1,10 @@
+import unittest
+from translator import englishToFrench, frenchToEnglish
+
+class TestTranslator(unittest.TestCase):
+    def test_fToE(self):
+        
+    def test_EToF(self):
+
+if __name__ == '__main__':
+    unittest.main()
